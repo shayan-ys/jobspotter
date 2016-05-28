bf99232ba1e5de8a5410447e07959b2062131835
Shayan Yousefian <shayan.yousefian1372@gmail.com>
2016-05-28 21:22:26 +0430

    Search for job ads

--- src/main/java/netgloo/models/AdsDao.java
+++ src/main/java/netgloo/models/AdsDao.java
@@ -1,8 +1,12 @@
 package netgloo.models;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * A DAO for the entity User is simply created by extending the CrudRepository
@@ -22,5 +26,25 @@ public interface AdsDao extends CrudRepository<Ads, Long> {
    * @param email the user email.
    */
   public Ads findById(int id);
-
-} // class UserDao
+  
+  @Query(value = "SELECT * FROM Ads as t WHERE "
+	         + "t.ad_type LIKE CONCAT('%',:ad_type,'%')"
+	         + "AND (t.description LIKE CONCAT('%',:desc,'%')"
+	         + " OR t.job_title LIKE CONCAT('%',:job_title,'%'))"
+	         + "AND t.category LIKE CONCAT('%',:category,'%')"
+	         + "AND t.ostan LIKE CONCAT('%',:ostan,'%')"
+	         + "AND t.city LIKE CONCAT('%',:city,'%')"
+	         + "AND t.job_type LIKE CONCAT('%',:time_type,'%')"
+	         + "AND t.gender LIKE CONCAT('%',:gender,'%')"
+	         , nativeQuery= true)
+	 public List<Ads> search(
+	         @Param("ad_type") String ad_type, 
+	         @Param("desc") String desc, 
+	         @Param("job_title") String job_title,
+	         @Param("category") String category,
+	         @Param("ostan") String ostan,
+	         @Param("city") String city,
+	         @Param("time_type") String time_type,
+	         @Param("gender") String gender
+	         );
+} // class AdsDao
